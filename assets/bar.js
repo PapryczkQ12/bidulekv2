ec9eb9da23d5cd32c3112d6860f24c1263e238dc
PapryczkQ12 <pstryczek41@gmail.com>
2025-04-02 18:08:09 +0200

    Update bar.js

--- assets/bar.js
+++ assets/bar.js
@@ -1,8 +1,19 @@
-
 var params = new URLSearchParams(window.location.search);
 
-function sendTo(url){
-    location.href = `/${url}?` + params;
+function sendTo(page) {
+    const pages = {
+        home: 'home.html',
+        documents: 'documents.html',
+        services: 'services.html',
+        qr: 'qr.html',
+        more: 'more.html',
+    };
+
+    if (pages[page]) {
+        window.location.href = pages[page];
+    } else {
+        console.error(`Page "${page}" not found.`);
+    }
 }
 
 document.querySelectorAll(".bottom_element_grid").forEach((element) => {
